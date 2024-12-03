cf0bd0e03307a7d1f16d080e22b473252a1d7172
xXNiklasBlokXx69 <nrbl24@student.aau.dk>
2024-12-03 11:58:16 +0100

    m

--- functions.h
+++ functions.h
@@ -1,35 +1,36 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-// DEFINES
-#define STATES 51
+
+//
+//
+//Initialiserings-funktioner
+#define STATE_MAX 51
 #define POPULATION 10000
 #define CANDIDATES 3
-#define RACES 7
-#define GENDERS 2
-#define INCOME 5
-#define AGES 5
-#define MAX_NAME_LENGTH 20
 
-// ENUMS
-typedef enum { white, black, hispanic, asian, native_american, native_hawaiian, other } race_e;
-typedef enum { male, female } gender_e;
-typedef enum { poor, low, middle, high, rich } income_e;
-typedef enum { young, adult, middle_aged, old, elderly } age_e;
+typedef enum { white, black, hispanic, asian, native_american, native_hawaiian, other } race;
+typedef enum { male, female } gender;
+typedef enum { low, middle, high } income;
+typedef enum { young, adult, middle_aged, old, elderly } age;
 
-// STRUCTS
-typedef struct {
-    age_e age_v;
-    gender_e gender_v;
-    race_e race_v;
-    income_e income_v;
-    int is_voting;
-    double værdipolitik_v;
-    double fordelingspolitik_v;
+//VOTER STRUCT
+typedef struct
+{
+    age age_v; // ung 18-25. voksen 26-39. middelaldrene 40-59. ældre 60-69. gammel 70+?
+    gender gender_v; // 0=male, 1=female
+    race race_v;
+    income income_v; //by level: 0 = low, 1 = middle, 2 = high
+    int is_voting; //0 = not voting, 1 = voting
+    //politics:
+    double værdipolitik_v; // Value politics?/policy?
+    double fordelingspolitik_v; // Distribution Politics?/policy?
     int rankings[CANDIDATES]; // Rangering af kandidater (ranked)
 } voter;
 
-typedef struct {
+//CANDIDATE STRUCTF
+typedef struct
+{
     char name[25];
     double værdipolitik_c; // Value politics?/policy?
     double fordelingspolitik_c; // Distribution Politics?/policy?
@@ -40,41 +41,43 @@ typedef struct {
     int eliminated; // Flag for elimineret kandidat (ranked)
 } candidate;
 
-typedef struct {
-    char name[MAX_NAME_LENGTH];
-    int race_distribution[RACES];
-    int gender_distribution[GENDERS];
-    int income_distribution[INCOME];
-    int age_distribution[AGES];
-    int population;
-    int electoral_votes;
+//STATE STRUCT
+typedef struct
+{
+    char name[20];
+    int votes_fptp;
+    int votes_star;
+    int votes_rated;
+    int votes_rcv;
+    int voters_population;
+    int electoral_mandates;
 } state;
 
 
-// Initalization functions
+
+//initalization functions
 void init_state(state state_arr[]); //DONE
-void init_voters(state current_state, voter voters_arr[], int attribute[]);
-void init_attributes(int state_population, voter voters_arr[], int attribute[], int attribute_options, int distribution[], int attribute_type);
+void init_voters(voter voters_arr[]);
 void init_candidates(candidate candidate_arr[]); //DONE
 
-// Voting system functions
+//Voting system functions
 void voting_fptp(state current_state, voter voters_arr[], candidate candidate_arr[]);
 void voting_star(state current_state, voter voters_arr[], candidate candidate_arr[]);
 void voting_rated(state current_state, voter voters_arr[], candidate candidate_arr[]);
 void voting_rcv(state current_state, voter voters_arr[], candidate candidate_arr[]);
 
-// Winner functions
+//winner functions
 candidate find_winner_fptp();
 candidate find_winner_star();
 candidate find_winner_rated();
 candidate find_winner_rcv();
 
-// Result function
-void print_winners();
+//result function
+void print_results();
 
 //
 //
-// Ranked functions
+// ranked functions
 int check_majority(candidate candidate_arr[], int total_voters);
 int find_lowest_votes(candidate candidate_arr[]);
 void redistribute_votes(voter voters_arr[], candidate candidate_arr[], int eliminated_candidate);
@@ -82,34 +85,27 @@ void reset_votes(candidate candidate_arr[]);
 
 void start_ranked_voting(candidate candidate_arr[], voter voters_arr[],int total_voters,int eliminated_candidate);
 
+
 //
 //
-// Rated functions
+// rated functions
 typedef struct {
     int total;
     int trump[10];
     int harris[10];
-} votes_rated;
+} votes;
 
-typedef struct {
-    int mandates;
-    votes_rated v;
-} state_rated;
-
-void get_votes(FILE *f, votes_rated *v);
-void results(votes_rated *v);
-//double median(int votes[], int size);
+void get_votes(FILE *f, votes *v);
+void results(votes *v);
+double median(int votes[], int size);
 double average(int votes[], int size);
 
 //
 //
-// FPTP functions
+// fptp functions
 void print_results(candidate winner);
 candidate find_winner_fptp(candidate candidate_arr[]);
 void voting_fptp(state current_state, voter voters_arr[], candidate candidate_arr[]);
 void start_fptp_voting(state state_arr[], voter voters_arr[], candidate candidate_arr[]);
-
-// Misc.
-void print_percent(double calc_percent[][7]);
 
 #endif //FUNCTIONS_H
