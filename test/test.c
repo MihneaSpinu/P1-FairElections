875ecc53b257b34bd3f8a77c9dd29ce0d796ee3d
nikla <niklasbloklarsen2@gmail.com>
2024-12-12 17:03:10 +0100

    test er færdig, og "testet"

--- test/test.c
+++ test/test.c
@@ -1,11 +1,13 @@
 #include <assert.h>
-#include <stdio.h>
-#define DELTA 0.001f
-#include <tgmath.h>
+#include <stdio.h>'
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#define DELTA 0.001
 
 #include "functions.h"
 void test_1();
-void init_test(voter voter_arr[], candidate candidate_arr[], state *test_state);
+void init_test(voter voter_arr[], candidate candidate_arr[]);
 int are_floats_equal(float a, float b);
 int main(void) {
     test_1();
@@ -16,18 +18,23 @@ void test_1() {
     state test_state;
     voter voter_arr[5];
     candidate candidate_arr[3];
-    init_test(voter_arr, candidate_arr, &test_state);
-    get_distance(voter_arr, candidate_arr, 5, 2);
+    init_test(voter_arr, candidate_arr);
+    get_distance(voter_arr, candidate_arr, 5, 3);
     assert(are_floats_equal(voter_arr[0].distance_to[0], 55.172f));
     assert(are_floats_equal(voter_arr[0].distance_to[1], 58.0f));
+    assert(are_floats_equal(voter_arr[0].distance_to[2], 14.866f));
     assert(are_floats_equal(voter_arr[1].distance_to[0], 49.497f));
     assert(are_floats_equal(voter_arr[1].distance_to[1], 63.639f));
+    assert(are_floats_equal(voter_arr[1].distance_to[2], 20.518f));
     assert(are_floats_equal(voter_arr[2].distance_to[0], 72.801f));
     assert(are_floats_equal(voter_arr[2].distance_to[1], 60.828f));
+    assert(are_floats_equal(voter_arr[2].distance_to[2], 35.228f));
     assert(are_floats_equal(voter_arr[3].distance_to[0], 72.112f));
     assert(are_floats_equal(voter_arr[3].distance_to[1], 44.722f));
+    assert(are_floats_equal(voter_arr[3].distance_to[2], 14.866f));
     assert(are_floats_equal(voter_arr[4].distance_to[0], 91.923f));
     assert(are_floats_equal(voter_arr[4].distance_to[1], 21.213f));
+    assert(are_floats_equal(voter_arr[4].distance_to[2], 21.932f));
 
     int winner_fptp = first_past_the_post(voter_arr, 5, 0, &test_state,3);
     assert(winner_fptp == 2);
@@ -38,7 +45,8 @@ void test_1() {
     int winner_star = voting_star(5, voter_arr, candidate_arr, 0, &test_state, 3);
     assert(winner_star == 2);
 }
-void init_test(voter voter_arr[], candidate candidate_arr[], state *test_state) {
+
+void init_test(voter voter_arr[], candidate candidate_arr[]) {
     voter_arr[0].værdipolitik_v = 12;
     voter_arr[1].værdipolitik_v = 15;
     voter_arr[2].værdipolitik_v = 30;
@@ -54,12 +62,9 @@ void init_test(voter voter_arr[], candidate candidate_arr[], state *test_state) 
     candidate_arr[2].fordelingspolitik_c = 0;
     candidate_arr[0].værdipolitik_c = 50;
     candidate_arr[1].værdipolitik_c = -30;
-    candidate_arr[2].værdipolitik_c = 0;
-    for (int i = 0; i < 2; i++) {
-        test_state->candidate_votes_fptp[i] = 0;
-    }
+    candidate_arr[2].værdipolitik_c = 1;
 }
 int are_floats_equal(float a, float b) {
     // Compare the absolute difference with the threshold
-    return fabs(a - b) < DELTA;
+    return fabs((double)a - (double)b) < DELTA;
 }
