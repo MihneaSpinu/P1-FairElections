54b2148323e9a3464d55d475c8d1cc250751f9f8
Chrillermm <cmunch24@student.aau.dk>
2024-12-12 10:58:08 +0100

    ;) OwO UwU kawaii

--- rated.c
+++ rated.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-
 #include "functions.h"
 
 int voting_rated(voter voter_arr[], int state_population, int start_index, state *current_state, int num_of_candidates) {
@@ -8,20 +7,14 @@ int voting_rated(voter voter_arr[], int state_population, int start_index, state
     for(int i = 0; i < num_of_candidates; i++) {
         votes_can[i] = 0;
     }
-    for (int i = start_index; i < state_population + start_index; i++) {
-        for (int j = 0; j < num_of_candidates; j++) {
-            get_ratings(voter_arr, i, j);
-        }
-    }
 
     for (int i = start_index; i < state_population + start_index; i++) {
         for (int j = 0; j < num_of_candidates; j++) {
+            get_ratings(voter_arr, i, j);
             votes_can[j] += voter_arr[i].ratings[j];
             current_state->candidate_votes_rated[j] += voter_arr[i].ratings[j];
         }
     }
-
-
 
     int winner = 0;
     for (int i = 0; i < num_of_candidates; i++) {
